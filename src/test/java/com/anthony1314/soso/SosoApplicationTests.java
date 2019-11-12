816841934ac83f8a3a809398319af41aa7634201
Cheneysboy <2316741037@qq.com>
2019-11-12 22:51:27 +0800

    区域信息业务逻辑完成

---
+++ src/test/java/com/anthony1314/soso/SosoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthony1314.soso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
